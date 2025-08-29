b0b6363db3e967fa112eab1a5a83adf395f67641
Proshanto Kumar <proshantokumar621@gmail.com>
2025-08-29 09:23:51 +0600

    Added heart count functionality

--- scripts.js
+++ scripts.js
@@ -66,12 +66,12 @@ const numbers = [{
 
 function createEmergencyCard(emergency) {
     return `
-        <div class="shadow-md min-w-[350px] min-h-[350px] w-3/10 border-2 bg-white p-4 rounded-2xl flex flex-col justify-between">
+        <div class="shadow-md w-[300px] h-[300px] border-2 bg-white p-4 rounded-2xl flex flex-col justify-between">
             <div class="flex items-center justify-between">
                 <div class="rounded-2xl bg-${emergency.iconBackground} w-[60px] h-[60px] flex items-center justify-center">
                     <img src="${emergency.iconPath}" class="w-[50px] h-[50px]">
                 </div>
-                <div>
+                <div class="heart-button">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="black" class="size-[3em]"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
                 </div>
             </div>
@@ -108,4 +108,17 @@ const numbersList = document.getElementById("num-list");
 
 numbers.forEach(emergency => {
     numbersList.innerHTML += createEmergencyCard(emergency);
+});
+
+// Select all heart buttons
+const heartButtons = document.querySelectorAll(".heart-button");
+// console.log(heartButtons);
+
+heartButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const currentCount = parseInt(document.getElementById("heart-count").innerText);
+        // console.log(currentCount);
+        heartCount = currentCount + 1;
+        document.getElementById("heart-count").innerText = heartCount;
+    });
 });
